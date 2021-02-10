5ab85955831d7a2ac887ec1ee5459f0a346ff6de
Andreea Mircioaga <andreea.mircioaga@innobyte.com>
2021-02-10 11:07:25 +0200

    deleted comments

---
+++ src/main/java/org/example/RadioControlledCar.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class RadioControlledCar {
+
+    String name;
+    String color;
+    int doorCount;
+    int wheelCount;
+    double mileage;
+    Engine engine;
+
+}
